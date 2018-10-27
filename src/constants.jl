8e2cd624a7e32d0c3931270be87aa1b89846e866
asieev <41776220+asieev@users.noreply.github.com>
2018-10-26 22:09:43 -0700

    Update src/constants.jl

--- src/constants.jl
+++ src/constants.jl
@@ -1,4 +1,4 @@
 export sets
 
-const sets = [:GRN, :M19, :DOM, :RIX, :XLN]
+const sets = [:GRN, :M19, :DOM, :RIX, :XLN, :HOU, :AKH, :AER, :KLD, :EMN, :SOI, :OGW, :BFZ]
 const set_counter = Dict( (s,0.0) for s in sets )
