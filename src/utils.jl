a19be3b7b35c22122593f2a80624a60845737bfe
asieev <41776220+asieev@users.noreply.github.com>
2018-10-28 22:52:06 -0700

    Multiple deck reduction

--- src/utils.jl
+++ src/utils.jl
@@ -25,6 +25,28 @@ function deckinfo(cardname, amount::Int; card_db = card_db)::NamedTuple{(:name, 
      prints = map(i -> (index = i, set = card_db[i]["set"], rarity = card_db[i]["rarity"]), prints))
 end
 
+
+function reduce_deck(deck; additive = true, card_db = card_db)
+    reduced = similar(deck, 0)
+
+    for elem in deck
+        ix = findfirst(x -> x.name == elem.name, reduced)
+
+        if ix == nothing
+            push!(reduced, elem)
+        else
+            if additive
+                reduced[ix] = deckinfo(elem.name, min(4, reduced[ix].amount + elem.amount); card_db = card_db)
+            else
+                reduced[ix] = deckinfo(elem.name, min(4, max(reduced[ix].amount, elem.amount)); card_db = card_db)
+            end
+        end
+    end
+
+    reduced
+end
+
+
 """
     deckinfo(deck::Vector{Tuple{String,Int}})
 
@@ -32,11 +54,12 @@ Processes a vector of card names and required quantities into a form that incorp
 set information, card rarity, and prints across multiple sets.
 """
 function deckinfo(deck::Vector{Tuple{String,Int}}; card_db = card_db)
-    map(x -> deckinfo(x[1], x[2]; card_db = card_db), deck)
+    reduce_deck(map(x -> deckinfo(x[1], x[2]; card_db = card_db), deck);
+     additive = true, card_db = card_db)
 end
 
-function deckinfo(x::Deckinfo)
-    x
+function deckinfo(x::Deckinfo; card_db = card_db)
+    reduce_deck(x; additive = true, card_db = card_db)
 end
 
 function icr_count(x)
